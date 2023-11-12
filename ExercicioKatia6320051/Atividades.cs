8c39d422195e9457d253cc1274e71564bb66eb25
Roni <Roni@MURILO>
2023-11-12 18:17:21 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioKatia6320051/Atividades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioKatia6320051
+{
+    public class Atividades
+    {
+        public string Nome { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+        public string Hora { get; set; }
+
+        public void Calcular()
+        {
+            Consumo c = new Consumo();
+            c.Calcular();
+        }
+
+    }
+}
---
+++ ExercicioKatia6320051/Consumo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioKatia6320051
+{
+    public class Consumo : Atividades 
+    {
+
+        public string Barco { get; set; }
+        public string Buggy { get; set; }
+        public string Trilha { get; set; }
+        
+        public void Calcular()
+        {
+          
+        }
+        // precisa incluir os atributos para que seja definido o valor de cada objeto, refri, suco, refeição feitas nos passeios
+    }
+}
